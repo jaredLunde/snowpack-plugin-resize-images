bde1523150a6ae035a07032fe0e0092e60a5b5d2
Jared Lunde <jared.lunde@gmail.com>
2020-08-11 10:14:15 -0600

    chore(release): 2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,12 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+## [2.0.0](https://github.com/jaredLunde/snowpack-plugin-resize-images/compare/v1.0.0...v2.0.0) (2020-08-11)
+
+### ⚠ BREAKING CHANGES
+
+- "images" option is now just the base options
+
+- remove superfluous images option ([d9dd451](https://github.com/jaredLunde/snowpack-plugin-resize-images/commit/d9dd451181fd7a609ddd95d49b6c58cac87b7c85))
+
 ## 1.0.0 (2020-08-11)
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,14 @@
-export interface SnowpackPluginResizeImagesOptions {
-  /**
-   * This is a mapping of glob patterns and their sharp methods
-   * and options. See the Sharp documentation for a complete list of
-   * methods and their respective options.
-   *
-   * @see https://sharp.pixelplumbing.com/api-output
-   */
-  images: {
-    [globPattern: string]: {
-      [sharpMethod: string]: {
-        [sharpMethodOption: string]: any
-      }
+/**
+ * This is a mapping of glob patterns and their sharp methods
+ * and options. See the Sharp documentation for a complete list of
+ * methods and their respective options.
+ *
+ * @see https://sharp.pixelplumbing.com/api-output
+ */
+export declare type SnowpackPluginResizeImagesOptions = {
+  [globPattern: string]: {
+    [sharpMethod: string]: {
+      [sharpMethodOption: string]: any
     }
   }
 }
